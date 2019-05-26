1f9dfca97a775609a8278a22d0e4dfcab3327b79
Michel Oleynik <michelole@gmail.com>
2019-05-26 20:46:14 +0200

    Responsive images
    Use the `gulp-responsive` tool to generate smaller, optimized, and WebP versions of images from the website. To separate source and target images, move source images to the `images` directory and use `img` for the responsive versions. Add original images to `images` dir.
    
    Use the `<picture>` HTML element and media queries to let the browser decide for the proper version.
    
    Darken the header background image in CSS using `background-blend-mode` instead of requiring post-processing with other tools.
    
    For the portfolio, use small and large versions instead of "full" and "thumbnail", which allow small devices to download a single "small" image for both the outer and inner visualizations.
    
    Support for WebP images in CSS will be considered in future.

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const responsive = require('gulp-responsive');
 
 // Load package.json for banner
 const pkg = require('./package.json');
@@ -113,21 +114,107 @@ function js() {
     .pipe(browsersync.stream());
 }
 
+// Optimize images task
+function image() {
+    const config = {
+        'portfolio/*.jpg': [
+            {   // jpg
+                width: 700,
+                rename: { suffix: '-large', extname: '.jpg' }
+            }, {
+                width: 700 * 2,
+                rename: { suffix: '-large@2x', extname: '.jpg' }
+            }, {
+                width: 400,
+                rename: { suffix: '-small', extname: '.jpg' }
+            }, {
+                width: 400 * 2,
+                rename: { suffix: '-small@2x', extname: '.jpg' }
+            }, {   // webp
+                width: 700,
+                rename: { suffix: '-large', extname: '.webp' }
+            }, {
+                width: 700 * 2,
+                rename: { suffix: '-large@2x', extname: '.webp' }
+            }, {
+                width: 400,
+                rename: { suffix: '-small', extname: '.webp' }
+            }, {
+                width: 400 * 2,
+                rename: { suffix: '-small@2x', extname: '.webp' }
+            } ],
+        'team/*.jpg': [
+            {   // jpg
+                width: 250,
+                rename: { suffix: '-small', extname: '.jpg' }
+            }, {
+                width: 250 * 2,
+                rename: { suffix: '-small@2x', extname: '.jpg' }
+            }, {    // webp
+                width: 250,
+                rename: { suffix: '-small', extname: '.webp' }
+            }, {
+                width: 250 * 2,
+                rename: { suffix: '-small@2x', extname: '.webp' }
+            } ],
+        'header-bg.jpg': [
+            {
+                width: 1440,
+                rename: { suffix: '-large', extname: '.jpg' }
+            }, {
+                width: 2848,    // Do not enlarge
+                rename: { suffix: '-large@2x', extname: '.jpg' }
+            }, {
+                width: 500,
+                rename: { suffix: '-small', extname: '.jpg' }
+            }, {
+                width: 500 * 2,
+                rename: { suffix: '-small@2x', extname: '.jpg' }
+            } ],
+        'map-image.png': [
+            {
+                width: 1440,
+                rename: { suffix: '-large', extname: '.png' }
+            },
+            // Original image is too small for a @2x version
+            {
+                width: 500,
+                rename: { suffix: '-small', extname: '.png' }
+            }, {
+                width: 500 * 2,
+                rename: { suffix: '-small@2x', extname: '.png' }
+            } ]
+    };
+
+    return gulp
+        .src([
+            './images/**/*.{jpg,png}'
+        ])
+        .pipe(responsive(config, {
+            quality: 85,
+            progressive: true,
+            compressionLevel: 9
+        }))
+        .pipe(gulp.dest('./img'))
+}
+
 // Watch files
 function watchFiles() {
   gulp.watch("./scss/**/*", css);
   gulp.watch("./js/**/*", js);
+  gulp.watch("./images/**/*", image);
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, gulp.parallel(css, js, image));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
+exports.image = image;
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
